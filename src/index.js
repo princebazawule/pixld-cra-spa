1dd701ad3139926d26b773af45dad6a3c3236ab4
Prince Bazawule <prince.bazawule@gmail.com>
2021-10-01 11:09:30 -0400

    add pwa support

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { hydrate, render } from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.scss';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const rootElement = document.getElementById("root");
 
@@ -14,6 +15,10 @@ if (rootElement.hasChildNodes()) {
     </Router>,
     rootElement
   );
+  // If you want your app to work offline and load faster, you can change
+  // unregister() to register() below. Note this comes with some pitfalls.
+  // Learn more about service workers: https://cra.link/PWA
+  serviceWorkerRegistration.register();
 } else {
   render(
     <Router>
